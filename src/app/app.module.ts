32074f973c8affa8478a2795c910b2ac4741e0db
AGvozd <AGvozd@its.jnj.com>
2021-07-12 00:41:48 +0300

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HeaderComponent,
+      LeftComponent,
+      RightComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
